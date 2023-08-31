990c8a790453f8b3e557c3e27735de900260909d
Thashmi03 <thashmigaa.cs20@bitsathy.ac.in>
2023-08-31 15:59:53 +0530

    x

--- netxd_customer_controller/netxd_customer_controller.go
+++ netxd_customer_controller/netxd_customer_controller.go
@@ -4,10 +4,8 @@ import (
 	netxddalinterface "github.com/Thashmi03/netxd_dal/netxd_dal_interface"
 	netxddalmodels "github.com/Thashmi03/netxd_dal/netxd_dal_models"
 	"context"
-	// tmodel "github.com/Thashmi03/transfer_model"
-	tinterface "github.com/Thashmi03/transfer_interface"
 	c "github.com/Thashmi03/netxd_customer"
-	// t "github.com/Thashmi03/netxd_transfer"
+	
 )
 type RPCServer struct{
 	c.UnimplementedCustomerServiceServer
@@ -15,7 +13,7 @@ type RPCServer struct{
 
 var(
 	CustomerService netxddalinterface.ICustomer
-	TransferService tinterface.Itransact
+	
 )
 
 func(s *RPCServer)CreateCustomer(ctx context.Context,req * c.Details)(*c.DetailResponse,error){
@@ -26,12 +24,7 @@ func(s *RPCServer)CreateCustomer(ctx context.Context,req * c.Details)(*c.DetailR
 		BankId:     req.BankId,
 		Balance:    req.Balance,
 	}
-	// dbTransfer:=&tmodel.Transaction{
-	// 	Transaction_id: "00001",
-	// 	From_account:   317,
-	// 	To_account:     318,
-	// 	Amount:         100,
-	// }
+	
 	res,err:=CustomerService.CreateCustomer(dbProfile)
 	
 	if err != nil {
